ec585a78c6092f88101c8c80596c5b492d34df1e
Brendan <112022206+brendanconroy9@users.noreply.github.com>
2024-12-05 20:47:36 -0600

    updated script 2

--- hardening_script2.sh
+++ hardening_script2.sh
@@ -8,3 +8,24 @@ echo "Gaterhing details from sshd configuration file"
 echo "sshd configuration file:$(cat /etc/ssh/sshd_config)" >> $REPORT_FILE
 print "\n" >> $REPORT_FILE
 
+# Update the packages and services
+
+echo "updating packages and services"
+sudo apt update && sudo apt upgrade -y
+echo "packages have been updated and upgraded" >> $REPORT_FILE
+printf "\n" >> $REPORT_FILE
+
+# List all installed packages
+
+echo "Installed Packages: $(dpkg --get-selections)" >> $REPORT_FILE
+printf "\n" >> $REPORT_FILE
+
+# Print logging config
+echo “Printing out logging configuration data”
+sudo journalctl
+echo "journald.conf file data: $(cat /e/tc/systemd/joournald.conf)" >> $REPORT_FILE
+printf "\n" >> $REPORT_FILE
+
+echo "logrotate.conf file data:$(cat /etc/logrotate.conf)" >> $REPORT_FILE
+printf "\n" >> $REPORT_FILE
+echo "Script execution completed. Check $REPORT_FILE for details."
